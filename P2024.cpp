a6ba5bc13e2efb207dd2795ab7be945059b5db35
wzcwzc05 <41479572+wzcwzc05@users.noreply.github.com>
2022-03-29 09:52:10 +0800

    Update P2024.cpp

--- P2024.cpp
+++ P2024.cpp
@@ -1,33 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 //滑动窗口
-class Solution
-{
-public:
-    int maxConsecutiveAnswers(string answerKey, int k)
+    class Solution
     {
-        int n = answerKey.length();
-        int left = 0, right = 0;
-        int ans = 0;
-        int tt, tf;
-        tt = 0;
-        tf = 0;
-        if (answerKey[0] == 'F')
-            tf = 1;
-        else
-            tt = 1;
-        while (left <= n - 1)
+    public:
+        int maxConsecutiveAnswers(string answerKey, int k)
         {
-            if ((tt + k >= right - left + 1) || (tf + k >= right - left + 1))
+            int n = answerKey.length();
+            int left = 0, right = 0;
+            int ans = 0;
+            int tt, tf;
+            tt = 0;
+            tf = 0;
+            if (answerKey[0] == 'F')
+                tf = 1;
+            else
+                tt = 1;
+            while (left <= n - 1)
             {
-                ans = max(ans, right - left + 1);
-                if (right <= n - 2)
+                if ((tt + k >= right - left + 1) || (tf + k >= right - left + 1))
                 {
-                    right++;
-                    if (answerKey[right] == 'F')
-                        tf++;
+                    ans = max(ans, right - left + 1);
+                    if (right <= n - 2)
+                    {
+                        right++;
+                        if (answerKey[right] == 'F')
+                            tf++;
+                        else
+                            tt++;
+                    }
                     else
-                        tt++;
+                    {
+                        left++;
+                        if (answerKey[left - 1] == 'F')
+                            tf--;
+                        else
+                            tt--;
+                    }
                 }
                 else
                 {
@@ -38,18 +47,9 @@ public:
                         tt--;
                 }
             }
-            else
-            {
-                left++;
-                if (answerKey[left - 1] == 'F')
-                    tf--;
-                else
-                    tt--;
-            }
+            return ans;
         }
-        return ans;
-    }
-};
+    };
 int main()
 {
     string st;
