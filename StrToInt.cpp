8217e5b22f6ad0aa92a070a208dd43c7d0ed0cc6
wzcwzc05 <wzcwzc0@outlook.com>
2022-03-30 09:20:54 +0800

    Update

---
+++ StrToInt.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int strToInt(string str)
+    {
+        int n = str.length() - 1;
+        int t = 1, ans=0;
+        bool flag = false;
+        for (int i = n; i >= 0; i--)
+        {
+            if (str[i] == '-')
+            {
+                ans = -ans;
+                break;
+            }
+            else if (str[i] >= '1' && str[i] <= '9')
+            {
+                flag=true;
+                int tmp = (int)str[i];
+                tmp -= 48;
+                ans += tmp * t;
+                t *= 10;
+            }
+            else if (flag == false)
+                continue;
+                else break;
+        }
+        return ans;
+    }
+};
+int main()
+{
+    Solution ans;
+    string st;
+    cin >> st;
+    cout << ans.strToInt(st) << endl;
+    return 0;
+}
