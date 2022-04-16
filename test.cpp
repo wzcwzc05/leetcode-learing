601054ed3dbd3d50eb3e3ba0638d968c3be578b2
wzcwzc05 <wzcwzc0@outlook.com>
2022-04-16 20:15:45 +0800

    Update

--- test.cpp
+++ test.cpp
@@ -1,26 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
-int a[100002],dp[1000002];
-int dep,n;
-const int MAX= 1000002;
-int main(){
-    scanf("%d",&n);
-    for (int i=1;i<=n;i++){
-        scanf("%d",&a[i]);
+int a[10002], t[10002], dp[10002][10002];
+int dep, n, m;
+const int MAX = 1000002;
+int main()
+{
+    scanf("%d %d", &n, &m);
+    for (int i = 1; i <= m; i++)
+    {
+        scanf("%d %d", &t[i], &a[i]);
     }
-    memset(dp,0,sizeof(dp));
-    for (int i=1;i<=n;i++){
-        dp[i]=1;
-        for (int j=1;j<i;j++){
-            if (a[i]>a[j]){
-                dp[i]=max(dp[i],dp[j]+1);
+    for (int i = 1; i <= m; i++)
+    {
+        for (int j = n; j >= 0; j--)
+        {
+            if (j - t[i] >= 0)
+            {
+                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - t[i]] + a[i]);
+            }
+            else
+            {
+                dp[i][j] = dp[i - 1][j];
             }
         }
     }
-    int ans=0;
-    for (int i=1;i<=n;i++){
-        ans=max(ans,dp[i]);
+    int ans = 0;
+    for (int i = 0; i <= n; i++)
+    {
+        ans = max(ans, dp[m][i]);
     }
-    cout<<ans<<endl;
+    cout << ans << endl;
     return 0;
 }
